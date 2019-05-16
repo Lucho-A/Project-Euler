556881e8867fe3662df28a162b7a038fb146aada
Lucho-A <40904281+Lucho-A@users.noreply.github.com>
2019-05-15 21:35:20 -0300

    +P0120 TODO

---
+++ Project Euler/Source/Problems/P0120.c
@@ -0,0 +1,34 @@
+/*
+	Square remainders
+	Problem 120
+
+	L
+	15 may. 2019-20:42:43
+ */
+
+#include "libEuler.h"
+#define LIMIT_MAX_a 1000
+#define LIMIT_MIN_a 3
+#define EQ	((double)(pow((a-1),n) + pow((a+1),n)))
+
+void P0120(void){
+	time_t tInit=clock();
+	double a=0, n=0, r=0, cont=0, maxR=0, result=0;
+	for(a=LIMIT_MIN_a;a<=LIMIT_MAX_a;a++){
+		cont=0;
+		maxR=0;
+		for(n=0;;n++){
+			r=EQ%(a*a);
+			if(maxR<r) maxR=r;
+			(r==0)?(cont++):(cont=0);
+			if(cont==5){
+				printf("a: %.f, n: %.f, r: %.f, maxR: %.f\n",a,n,r, maxR);
+				result+=maxR;
+				break;
+			}
+		}
+	}
+	time_t tEnd=clock();
+	printf("Problem P0120 - Result: %.f. Elapsed Time: %.6f\n", result,(double) (tEnd-tInit)/CLOCKS_PER_SEC);
+	return;
+}
