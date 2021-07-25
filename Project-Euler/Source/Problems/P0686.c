390f04915f11935a2edd88fb1767790434ab2310
Lucho-A <lucho@lucho-L>
2021-07-25 18:51:07 -0300

    +p0686.C //TODO (ver de usar logaritmos)

--- Project-Euler/Source/Problems/P0686.c
+++ Project-Euler/Source/Problems/P0686.c
@@ -6,24 +6,33 @@
  */
 
 #include "libEuler.h"
-//#define N 678910
-#define N 2
+#define N 678910
+//#define N 45
 
-void P0686(void){
+void P0686(void){ // VER CON LOGARITMOS (https://www.geeksforgeeks.org/find-the-first-and-last-m-digits-from-k-th-power-of-n/)
 	time_t tInit=clock();
-	llu cont=0,j=0, r=2;
-	char strN[10000];
+	int cont=0,j=0, len=0, sum=0, carry=0;
+	char strN[100000]="2";
 	for(j=2;;j++){
-		r*=2;
-	 	sprintf(strN,"%llu",r);
-	 	printf("%s\n",strN);
-	 	if(strN[0]=='1' && strN[1]=='2'){
-			cont++;
+		len=0;
+		carry=0;
+		while(len<strlen(strN)){
+			sum=((strN[len] - '0') * 2 + carry);
+			strN[len]=(sum % 10) + '0';
+			(sum>9)?(carry=1):(carry=0);
+			len++;
 		}
-		if(cont==N){
-			time_t tEnd=clock();
-			printf("Problem P0686 - Result: %llu. Elapsed Time: %.6f\n", j,(double) (tEnd-tInit)/CLOCKS_PER_SEC);
-			return;
+		if(carry==1) strN[len]='1';
+		if(strN[strlen(strN)-1]=='1' && strN[strlen(strN)-2]=='2' && strN[strlen(strN)-3]=='3'){
+		//if(strN[strlen(strN)-1]=='1' && strN[strlen(strN)-2]=='2'){
+			cont++;
+			//printf("%s\n",strN);
+			printf("%d\n",cont);
+			if(cont==N){
+				time_t tEnd=clock();
+				printf("Problem P0686 - Result: %llu. Elapsed Time: %.6f\n", j,(double) (tEnd-tInit)/CLOCKS_PER_SEC);
+				return;
+			}
 		}
 	}
 	return;
