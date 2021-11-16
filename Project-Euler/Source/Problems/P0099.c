2fc8e0cc3c48cd76f525756cb1312b33d1f77341
Lucho-A <lucho@lucho-L>
2021-11-16 14:03:08 -0300

    + P0099: Solved

---
+++ Project-Euler/Source/Problems/P0099.c
@@ -0,0 +1,30 @@
+/*
+	Largest exponential
+
+	Problem 99
+
+	L.
+
+	16 nov. 2021
+*/
+
+#include "libEuler.h"
+
+void P0099(void){
+	time_t tInit=clock();
+	long int maxLine=1, contLine=1;
+	double maxValue=0.0, base=0.0, exp=0.0, r=0.0;
+	FILE *f=NULL;
+	if((f=fopen("Resources/P0099/p099_base_exp.txt","r"))==NULL) exit(EXIT_FAILURE);
+	while(fscanf(f,"%lf,%lf",&base,&exp)!=EOF){
+		r=exp*log10(base);
+		if(r>maxValue){
+			maxValue=r;
+			maxLine=contLine;
+		}
+		contLine++;
+	}
+	time_t tEnd=clock();
+	printf("Problem 99 - Result: %ld. Elapsed Time: %.6f\n", maxLine,(double) (tEnd-tInit)/CLOCKS_PER_SEC);
+	return;
+}
